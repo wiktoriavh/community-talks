96797afba534e0770b0b387678acd0894a718de3
Wiktoria van Harneveldt <62669899+Braweria@users.noreply.github.com>
2023-07-06 16:15:21 +0200

    feat: register Heading correctly

--- src/components/builder-registry.ts
+++ src/components/builder-registry.ts
@@ -1,21 +1,21 @@
-import type { RegisteredComponent } from "@builder.io/sdk-qwik";
-import { Heading } from "./typography/Heading";
+import type { RegisteredComponent } from '@builder.io/sdk-qwik';
+
+import { Heading } from './typography/Heading';
 
 export const CUSTOM_COMPONENTS: RegisteredComponent[] = [
   {
-    name: "Heading",
+    name: 'Heading',
     component: Heading,
     inputs: [
       {
-        name: "children",
-        type: "ui",
-        subFields: [
-          {
-            name: "text",
-            type: "string",
-            defaultValue: "Hello World",
-          },
-        ],
+        name: 'text',
+        type: 'string',
+        defaultValue: 'Hello World',
+      },
+      {
+        name: 'level',
+        type: 'number',
+        defaultValue: 1,
       },
     ],
   },
