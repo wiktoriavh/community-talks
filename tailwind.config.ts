e82f6d3530c93276da1669431df241195be5857f
Wiktoria van Harneveldt <62669899+Braweria@users.noreply.github.com>
2023-07-06 15:57:38 +0200

    chore: configure tailwid with colours and font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,21 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          500: '#D3E065',
+          50: '#FCFEEB',
+        },
+        dark: {
+          500: '#2F3035',
+        },
+      },
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+      heading: ['Montserrat Alternates', 'sans-serif'],
+    },
   },
   plugins: [forms, typography],
 } satisfies Config;
