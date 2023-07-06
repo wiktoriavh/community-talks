27e487378dfd7357496f4719ab27a10747d63475
Wiktoria van Harneveldt <62669899+Braweria@users.noreply.github.com>
2023-07-06 15:02:02 +0200

    chore: use path alias for components

--- src/routes/[...index]/index.tsx
+++ src/routes/[...index]/index.tsx
@@ -1,12 +1,12 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
-import { routeLoader$ } from "@builder.io/qwik-city";
+import { CUSTOM_COMPONENTS } from '@/builder-registry';
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import { routeLoader$ } from '@builder.io/qwik-city';
 import {
-  getContent,
   RenderContent,
   getBuilderSearchParams,
-} from "@builder.io/sdk-qwik";
-import { CUSTOM_COMPONENTS } from "~/components/builder-registry";
+  getContent,
+} from '@builder.io/sdk-qwik';
 
 // This page is a catch-all for all routes that don't have a pre-defined route.
 // Using a catch-all route allows you to dynamically create enw pages in Builder.
@@ -15,13 +15,13 @@ import { CUSTOM_COMPONENTS } from "~/components/builder-registry";
 // `routeLoader$()` takes an async function to fetch content
 // from Builder with using `getContent()`.
 export const useBuilderContent = routeLoader$(async ({ url, error }) => {
-  const isPreviewing = url.searchParams.has("builder.preview");
+  const isPreviewing = url.searchParams.has('builder.preview');
 
   // Fetch Builder.io Visual CMS content using the Qwik SDK.
   // The public API key is set in the .env file at the root
   // https://www.builder.io/c/docs/using-your-api-key
   const builderContent = await getContent({
-    model: "page",
+    model: 'page',
     apiKey: import.meta.env.PUBLIC_BUILDER_API_KEY,
     options: getBuilderSearchParams(url.searchParams),
     userAttributes: {
@@ -32,7 +32,7 @@ export const useBuilderContent = routeLoader$(async ({ url, error }) => {
   // If there's no content, throw a 404.
   // You can use your own 404 component here
   if (!builderContent && !isPreviewing) {
-    throw error(404, "Page not found");
+    throw error(404, 'Page not found');
   }
 
   // return content fetched from Builder, which is JSON
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,8 +1,7 @@
+import { Footer } from '@/navigation/Footer';
+import { Header } from '@/navigation/Header';
+import { Heading } from '@/typography/Heading';
 import { Slot, component$ } from '@builder.io/qwik';
-
-import { Footer } from '../components/navigation/Footer';
-import { Header } from '../components/navigation/Header';
-import { Heading } from '../components/typography/Heading';
 
 export default component$(() => {
   return (
