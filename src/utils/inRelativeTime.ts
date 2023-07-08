2e35f5aa8f41c3afffc03b3fda31605b87fe3f7f
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-07-08 18:59:24 +0400

    :white_check_mark: Adds tests

--- src/utils/inRelativeTime.ts
+++ src/utils/inRelativeTime.ts
@@ -16,3 +16,26 @@ const timeSizes: [Intl.RelativeTimeFormatUnit, number][] = [
   ['hour', 1000 * 60 * 60],
   ['minute', 1000 * 60],
 ];
+
+if (import.meta.vitest) {
+  describe('inRelativeTime', () => {
+    it('Returns the number of days until', () => {
+      expect(
+        inRelativeTime(new Date(Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000))
+      ).toBe('in 2 days');
+    });
+    it('Returns hours when less than a day', () => {
+      expect(
+        inRelativeTime(new Date(Date.now() + 1000 * 60 * 60 * 2 + 1000))
+      ).toBe('in 2 hr.');
+    });
+    it('Returns minutes when less than an hour', () => {
+      expect(inRelativeTime(new Date(Date.now() + 1000 * 60 * 2 + 1000))).toBe(
+        'in 2 min.'
+      );
+    });
+    it('Returns Now for the current time', () => {
+      expect(inRelativeTime(new Date())).toBe('Now');
+    });
+  });
+}
