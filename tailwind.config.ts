f73b3a93676b8a9c9b1d291dc41c4af5dcdf20dd
Wiktoria van Harneveldt <62669899+Braweria@users.noreply.github.com>
2023-07-08 19:09:33 +0200

    chore: create heading stylings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,53 @@ export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      typography: (theme: (styleName: string) => any) => ({
+        DEFAULT: {
+          css: {
+            h1: {
+              color: theme('colors.primary.500'),
+              fontFamily: theme('fontFamily.heading'),
+              fontWeight: theme('fontWeight.extrabold'),
+              fontSize: theme('fontSize.4xl'),
+            },
+            h2: {
+              color: theme('colors.white'),
+              fontFamily: theme('fontFamily.heading'),
+              fontWeight: theme('fontWeight.extrabold'),
+              fontSize: theme('fontSize.3xl'),
+            },
+            h3: {
+              color: theme('colors.white'),
+              fontFamily: theme('fontFamily.heading'),
+              fontWeight: theme('fontWeight.extrabold'),
+              fontSize: theme('fontSize.2xl'),
+            },
+            h4: {
+              color: theme('colors.white'),
+              fontFamily: theme('fontFamily.heading'),
+              fontWeight: theme('fontWeight.extrabold'),
+              fontSize: theme('fontSize.xl'),
+            },
+            h5: {
+              color: theme('colors.white'),
+              fontFamily: theme('fontFamily.heading'),
+              fontWeight: theme('fontWeight.bold'),
+              fontSize: theme('fontSize.xl'),
+            },
+            h6: {
+              color: theme('colors.white'),
+              fontFamily: theme('fontFamily.heading'),
+              fontWeight: theme('fontWeight.semibold'),
+              fontSize: theme('fontSize.xl'),
+            },
+            p: {
+              color: theme('colors.white'),
+              fontFamily: theme('fontFamily.sans'),
+            },
+          },
+        },
+      }),
+
       colors: {
         primary: {
           500: '#D3E065',
